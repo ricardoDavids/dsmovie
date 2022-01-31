df3677d47a0ab8166e9173ff8d8ce572395716ac
Ricardo David <ricardo.david2020.new@gmail.com>
2022-01-31 17:33:31 +0000

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,8 +2,24 @@ import MovieCard from "components/MovieCard";
 import axios from "axios";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing(){
+
+    const [pageNumber, setPageNumber] = useState(0); //Como a minha API retorna pagina 0 como sendo a 1ª, vou colocar entre parenteses o valor zero
+
+    useEffect(() =>{
+
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(responseRequisicao => {
+            const data =responseRequisicao.data as MoviePage;
+            console.log(data);//Console.log que é o console do navegador e data é um atributo e é onde vem o corpo da resposta e ai vou imprimir na consola do navegador
+            setPageNumber(data.number);
+           
+        }).catch(error => console.log(error)); //Se tua requisição der qualquer erro, ele vai  mostrar no console qual erro q foi
+
+    }, []);
 
     //Forma ERRADA
 
@@ -16,13 +32,12 @@ function Listing(){
     Por exemplo, para vc executar uma coisa depois que a requesição chegar á resposta dela, depois que chegar a resposta, vc tera que colocar na frente da chamada do get, a palavra then
     e depois o que colocar dentro do then vai ser executado depois que a requesição voltar,
     porque a requesição que vc faz aqui, ou seja, esse get pode demorar um tempo para chegar, ou seja, é uma operação assincrona, tudo continua executando mas quando chegar a resposta, entao ai eu vou querer executar uma coisa dentro do then.  */
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-            .then(responseRequisicao => {
-                console.log(responseRequisicao.data);//Console.log que é o console do navegador e data é um atributo e é onde vem o corpo da resposta e ai vou imprimir na consola do navegador
-            });
+   
+       
 
     return(
         <>
+            <p>{pageNumber}</p>
                 <Pagination />
 
                 <div className="container">
